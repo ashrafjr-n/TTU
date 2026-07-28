d8e32b51987a9c3a253b3809fd08b85eae082130
Ashraf <250116877+ashrafjr-n@users.noreply.github.com>
2026-07-28 23:39:39 +0400

    isAdmin

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,4 +60,9 @@ class User extends Authenticatable
     {
         return $this->role === 'doctor';
     }
+
+        public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
 }
